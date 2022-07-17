a0972b7232608f952d50c37c213f85fea27804ef
mkonstantin <konstantin.malyshev@indriver.com>
2022-07-17 10:51:43 +0900

    fix

--- internal/domain_layer/model/place.go
+++ internal/domain_layer/model/place.go
@@ -4,10 +4,3 @@ type Place struct {
 	ID   int64  `json:"id,omitempty" db:"id"`
 	Name string `json:"name,omitempty" db:"name"`
 }
-
-type PlaceRepository interface {
-	Create(place Place) (Place, error)
-	Read(id int64) (Place, error)
-	Update(place Place) (Place, error)
-	Delete(id int64) error
-}
