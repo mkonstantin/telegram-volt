1a7f3d906c01d5d66bf57fb1e4a2604c603a6098
mkonstantin <konstantin.malyshev@indriver.com>
2022-07-20 01:42:14 +0900

    add user model

--- internal/domain_layer/model/user.go
+++ internal/domain_layer/model/user.go
@@ -1,6 +1,33 @@
 package model
 
+import "time"
+
 type User struct {
-	ID   int64  `json:"id,omitempty" db:"id"`
-	Name string `json:"name,omitempty" db:"name"`
+	ID            int64
+	Name          string
+	officeID      int64
+	placeID       int64
+	wishGoToLunch bool
+	LunchTime     time.Time
+}
+
+func (u *User) IsOfficeChoosed() bool {
+	return u.officeID > 0
+}
+
+func (u *User) ChooseOffice(id int64) {
+	u.officeID = id
+}
+
+func (u *User) WantLunch() {
+	u.wishGoToLunch = true
+	u.LunchTime = time.Now()
+}
+
+func (u *User) AbortLunch() {
+	u.wishGoToLunch = false
+}
+
+func (u *User) ChoosePlace(id int64) {
+	u.placeID = id
 }
