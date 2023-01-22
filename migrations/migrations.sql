604d58230ea005921e373b1973e8fab55eb1cebb
mkonstantin <konstantin.malyshev@indriver.com>
2023-01-22 11:36:09 +0600

    add migrations

--- migrations/migrations.sql
+++ migrations/migrations.sql
@@ -1,3 +1,4 @@
+
 CREATE TABLE `user`
 (
     `id`                INT(11)               NOT NULL AUTO_INCREMENT,
@@ -16,9 +17,40 @@ CREATE TABLE `office`
 (
     `id`                INT(11)               NOT NULL AUTO_INCREMENT,
     `name`              VARCHAR(255)          NOT NULL,
-    `city_id`           INT(11)               NOT NULL,
+    `city`              VARCHAR(255)          NOT NULL,
     `created_at`        DATETIME              DEFAULT CURRENT_TIMESTAMP,
     `updated_at`        DATETIME              DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+)
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+
+CREATE TABLE `seat`
+(
+    `id`                INT(11)          NOT NULL AUTO_INCREMENT,
+    `seat_number`       INT(11)          NOT NULL,
+    `have_monitor`      BOOLEAN          DEFAULT FALSE,
+    `office_id`         INT(11)          NOT NULL,
+    `created_at`        DATETIME         DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`        DATETIME         DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+)
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+
+CREATE TABLE `book_seat`
+(
+    `id`                INT(11)          NOT NULL AUTO_INCREMENT,
+    `office_id`         INT(11)          NOT NULL,
+    `seat_id`           INT(11)          NOT NULL,
+    `user_id`           INT(11)          DEFAULT NULL,
+    `book_date`         DATETIME         NOT NULL,
+    `book_start_time`   DATETIME         DEFAULT NULL,
+    `book_end_time`     DATETIME         DEFAULT NULL,
+    `created_at`        DATETIME         DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`        DATETIME         DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
 )
     ENGINE = InnoDB
