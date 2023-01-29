26553e8f5bfbba26705ebed2aaf9d7c1202ab38e
mkonstantin <konstantin.malyshev@indriver.com>
2023-01-29 16:49:15 +0600

    dto

--- internal/app/usecase/dto/user.go
+++ internal/app/usecase/dto/user.go
@@ -8,6 +8,13 @@ type FirstStartDTO struct {
 	ChatID    int64
 }
 
+type BookSeatDTO struct {
+	TelegramID int64
+	OfficeID   int64
+	ChatID     int64
+	MessageID  int
+}
+
 type OfficeChosenDTO struct {
 	TelegramID int64
 	OfficeID   int64
@@ -19,6 +26,7 @@ type UserResult struct {
 	Key       string
 	Office    *model.Office
 	Offices   []*model.Office
+	BookSeats []*model.BookSeat
 	Message   string
 	MessageID int
 	ChatID    int64
