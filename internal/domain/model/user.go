5861725907a12ee6f0f13390dd49b98e21bfca52
mkonstantin <konstantin.malyshev@indriver.com>
2023-02-01 23:07:18 +0600

    user notify_office_id

--- internal/domain/model/user.go
+++ internal/domain/model/user.go
@@ -1,13 +1,18 @@
 package model
 
 type User struct {
-	ID           int64
-	Name         string
-	TelegramID   int64
-	TelegramName string
-	OfficeID     int64
+	ID             int64
+	Name           string
+	TelegramID     int64
+	TelegramName   string
+	OfficeID       int64
+	NotifyOfficeID int
 }
 
 func (u *User) HaveChosenOffice() bool {
 	return u.OfficeID != 0
+}
+
+func (u *User) isNeedNotify() bool {
+	return u.NotifyOfficeID > 0
 }
--- internal/infrastructure/repo/dto/user.go
+++ internal/infrastructure/repo/dto/user.go
@@ -6,21 +6,23 @@ import (
 )
 
 type User struct {
-	ID           int64     `db:"id,omitempty"`
-	Name         string    `db:"name,omitempty"`
-	TelegramID   int64     `db:"telegram_id,omitempty"`
-	TelegramName string    `db:"telegram_name,omitempty"`
-	OfficeID     *int64    `db:"office_id,omitempty"`
-	CreatedAt    time.Time `db:"created_at" json:"created_at"`
-	UpdatedAt    time.Time `db:"updated_at" json:"updated_at"`
+	ID             int64     `db:"id,omitempty"`
+	Name           string    `db:"name,omitempty"`
+	TelegramID     int64     `db:"telegram_id,omitempty"`
+	TelegramName   string    `db:"telegram_name,omitempty"`
+	OfficeID       *int64    `db:"office_id,omitempty"`
+	NotifyOfficeID int       `db:"notify_office_id,omitempty"`
+	CreatedAt      time.Time `db:"created_at" json:"created_at"`
+	UpdatedAt      time.Time `db:"updated_at" json:"updated_at"`
 }
 
 func (u *User) ToModel() *model.User {
 	user := model.User{
-		ID:           u.ID,
-		Name:         u.Name,
-		TelegramID:   u.TelegramID,
-		TelegramName: u.TelegramName,
+		ID:             u.ID,
+		Name:           u.Name,
+		TelegramID:     u.TelegramID,
+		TelegramName:   u.TelegramName,
+		NotifyOfficeID: u.NotifyOfficeID,
 	}
 	if u.OfficeID == nil {
 		user.OfficeID = 0
