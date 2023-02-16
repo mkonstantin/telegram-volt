61fcc0bb75d5096743de26750b7adc818644f50c
mkonstantin <konstantin.malyshev@indriver.com>
2023-02-16 22:34:25 +0600

    injects

--- cmd/main/inject_handlers.go
+++ cmd/main/inject_handlers.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"github.com/google/wire"
-	handler2 "telegram-api/internal/app/handler"
+	"telegram-api/internal/app/handler"
 )
 
 var handlerSet = wire.NewSet(
-	handler2.NewStartHandle,
-	handler2.NewOfficeListHandle,
-	handler2.NewOfficeMenuHandle,
-	handler2.NewSeatListHandle,
-	handler2.NewOwnSeatMenuHandle,
-	handler2.NewFreeSeatMenuHandle,
+	handler.NewStartHandle,
+	handler.NewOfficeListHandle,
+	handler.NewOfficeMenuHandle,
+	handler.NewSeatListHandle,
+	handler.NewOwnSeatMenuHandle,
+	handler.NewFreeSeatMenuHandle,
+	handler.NewDateMenuHandle,
 )
