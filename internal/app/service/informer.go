2201528790b7b20a82d1ff37f3673c85d254f939
mkonstantin <konstantin.malyshev@indriver.com>
2023-03-02 08:26:45 +0600

    informer send messages

--- internal/app/service/informer.go
+++ internal/app/service/informer.go
@@ -3,8 +3,11 @@ package service
 import (
 	"context"
 	"fmt"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"go.uber.org/zap"
+	"log"
 	"telegram-api/internal/infrastructure/repo/interfaces"
+	"telegram-api/internal/infrastructure/service"
 )
 
 type InformerService interface {
@@ -12,26 +15,77 @@ type InformerService interface {
 }
 
 type informerServiceImpl struct {
+	botAPI       *tgbotapi.BotAPI
+	officeRepo   int
+	userRepo     interfaces.UserRepository
 	bookSeatRepo interfaces.BookSeatRepository
 	logger       *zap.Logger
 }
 
-func NewInformer(bookSeatRepo interfaces.BookSeatRepository, logger *zap.Logger) InformerService {
+func NewInformer(botAPI *tgbotapi.BotAPI, userRepo interfaces.UserRepository,
+	bookSeatRepo interfaces.BookSeatRepository, logger *zap.Logger) InformerService {
 	return &informerServiceImpl{
+		botAPI:       botAPI,
+		userRepo:     userRepo,
 		bookSeatRepo: bookSeatRepo,
 		logger:       logger,
 	}
 }
 
-func (i *informerServiceImpl) SeatComeFree(ctx context.Context, id int64) error {
-	//currentUser := model.GetCurrentUser(ctx)
+func (i *informerServiceImpl) SeatComeFree(ctx context.Context, bookSeatID int64) error {
 
-	seat, err := i.bookSeatRepo.FindByID(id)
+	bookSeat, err := i.bookSeatRepo.FindByID(bookSeatID)
 	if err != nil {
 		return err
 	}
 
-	fmt.Println(seat.BookDate)
+	todayUTC := service.TodayZeroTimeUTC()
+	currentTime, err := service.CurrentTimeWithTimeZone(bookSeat.Office.TimeZone)
+	eveningTime, err := service.EveningTimeWithTimeZone(bookSeat.Office.TimeZone)
+
+	if err != nil {
+		i.logger.Error("Error TodayWithTimeZone", zap.Error(err))
+		// Ошибку не возвращаем, show must go on
+		//return err
+	}
+
+	if todayUTC == bookSeat.BookDate && currentTime.After(eveningTime) {
+		return nil
+	}
+	return i.sendNotifies(ctx, bookSeat.Office.ID)
+
+	//if todayUTC == bookSeat.BookDate {
+	//	currentTime, err := service.CurrentTimeWithTimeZone(bookSeat.Office.TimeZone)
+	//	eveningTime, err := service.EveningTimeWithTimeZone(bookSeat.Office.TimeZone)
+	//	if err != nil {
+	//		i.logger.Error("Error TodayWithTimeZone", zap.Error(err))
+	//		// Ошибку не возвращаем, show must go on
+	//		//return err
+	//	}
+	//
+	//	if currentTime.Before(eveningTime) {
+	//		i.sendNotifies(ctx)
+	//	}
+	//} else {
+	//	i.sendNotifies(ctx)
+	//}
+	//return nil
+}
+
+func (i *informerServiceImpl) sendNotifies(ctx context.Context, officeID int64) error {
+	users, err := i.userRepo.GetUsersToNotify(officeID)
+	if err != nil {
+		return err
+	}
+
+	for _, user := range users {
+		msg := tgbotapi.NewMessage(user.ChatID, "Освободилось место")
+		fmt.Println(msg)
+		if _, err = i.botAPI.Send(msg); err != nil {
+			log.Printf("Error when try to send message %d", err)
+			continue
+		}
+	}
 
 	return nil
 }
