7dbefbd5ced0fb9081ce4d95c34392618525a19d
mkonstantin <konstantin.malyshev@indriver.com>
2023-03-02 07:43:43 +0600

    sd

--- internal/infrastructure/service/time_helper.go
+++ internal/infrastructure/service/time_helper.go
@@ -4,6 +4,28 @@ import (
 	"time"
 )
 
+const eveningHour = 18
+
+func EveningTimeWithTimeZone(timeZone string) (time.Time, error) {
+	loc, err := time.LoadLocation(timeZone)
+	if err != nil {
+		return TodayZeroTimeUTC(), err
+	}
+	date := time.Now()
+	bookDate := time.Date(date.Year(), date.Month(), date.Day(), eveningHour, 0, 0, 0, loc)
+	return bookDate, nil
+}
+
+func CurrentTimeWithTimeZone(timeZone string) (time.Time, error) {
+	loc, err := time.LoadLocation(timeZone)
+	if err != nil {
+		return TodayZeroTimeUTC(), err
+	}
+	date := time.Now().In(loc)
+	bookDate := time.Date(date.Year(), date.Month(), date.Day(), date.Hour(), date.Minute(), 0, 0, loc)
+	return bookDate, nil
+}
+
 func TodayZeroTimeUTC() time.Time {
 	date := time.Now()
 	bookDate := time.Date(date.Year(), date.Month(), date.Day(), 0, 0, 0, 0, time.UTC)
