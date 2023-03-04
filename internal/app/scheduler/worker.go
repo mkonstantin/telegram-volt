fbfe27053e1f39e7acca73f3764047cfe529365c
mkonstantin <konstantin.malyshev@indriver.com>
2023-03-04 10:37:49 +0600

    kll

--- internal/app/scheduler/worker.go
+++ internal/app/scheduler/worker.go
@@ -54,23 +54,21 @@ func (w *jobsSchedulerImpl) createForOffice(office *model.Office) error {
 
 	s := gocron.NewScheduler(location)
 	_, err = s.Every(1).
-		//Minute().
-		Week().
-		At("14:30").
-		Weekday(time.Sunday).
-		Weekday(time.Monday).
-		Weekday(time.Tuesday).
-		Weekday(time.Wednesday).
-		Weekday(time.Thursday).
+		Day().
+		At("22:00").
 		Do(func() {
-			err = w.officeJobs.SetNewSeatList(office.ID, location)
+			year, week := time.Now().ISOWeek()
+
+			err = w.officeJobs.SetSeatsWeek(office.ID, year, week)
 			if err != nil {
 				w.logger.Error("gocron execution error", zap.Error(err))
 			}
 		})
+
 	if err != nil {
 		w.logger.Error("gocron create error", zap.Error(err))
 	}
+	s.StartImmediately()
 	s.StartAsync()
 
 	w.logger.Info(fmt.Sprintf("Success start async scheduled jobs for %s", office.Name))
