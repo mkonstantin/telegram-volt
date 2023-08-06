a6beb14b6c124e23a6ab07337f96abbde6bd903c
Konstantin Malyshev <konstantin.malyshev@indriver.com>
2023-08-06 17:46:21 +0600

    создание БД для Кипра

---
+++ init-scripts/create_databases.sql
@@ -0,0 +1,4 @@
+-- init-scripts/create_databases.sql
+CREATE DATABASE IF NOT EXISTS volt;
+CREATE DATABASE IF NOT EXISTS volt_cy;
+-- Добавьте здесь другие SQL-запросы для создания баз данных, если требуется
