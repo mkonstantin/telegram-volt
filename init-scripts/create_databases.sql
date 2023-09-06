2a29326bf1d81b0dab0d83b6e1b3c0c0af95d06b
Konstantin Malyshev <konstantin.malyshev@indriver.com>
2023-09-06 10:19:14 +0600

    fix

--- init-scripts/create_databases.sql
+++ init-scripts/create_databases.sql
@@ -1,4 +1,5 @@
 -- init-scripts/create_databases.sql
 CREATE DATABASE IF NOT EXISTS volt;
 CREATE DATABASE IF NOT EXISTS volt_cy;
+CREATE DATABASE IF NOT EXISTS volt_pf;
 -- Добавьте здесь другие SQL-запросы для создания баз данных, если требуется
